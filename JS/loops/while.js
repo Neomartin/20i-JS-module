ffa1eb36b64d9bd328fbc240df985783d57a2b98
Martín Rodríguez <neomartinr@gmail.com>
2021-11-01 20:45:22 -0300

    ejercicio escuela

--- JS/loops/while.js
+++ JS/loops/while.js
@@ -1,16 +1,47 @@
-let loops = 5;
+
+let totalAlumnos = parseInt(prompt("Ingrese el número de alumnos"))
 let i = 1;
-let total = 0;
-while(i <= loops) {
-    // document.write("Veces que se ejecuta el bucle:" + i + "<br>");
-    total = total + i;
+let promedioTotal = 0
+let promedioMatematica = 0
+let promedioLengua = 0
+let promedioCsSc = 0
+while(i <= totalAlumnos) {
+    let notaMatematica = +prompt("Ingrese la nota de matemática"); 
+    let notaLengua = +prompt("Ingrese la nota de lengua");
+    let notaCsSc = +prompt("Ingrese la nota de Cs Sociales");
+
+    document.write(`ALUMNO ${i} <br>  ================================== <br>`);
+
+    document.write(`La nota ingresada de matemática es ${notaMatematica} - `);
+    if(notaMatematica < 7) {
+        document.write(`DESAPROBÓ <br>`);
+    } else {
+        document.write(`APROBÓ <br>`);
+    }
+
+    document.write(`La nota ingresada de Lengua es ${notaLengua} - `);
+    // Operador ternario
+    notaLengua < 7 ? document.write(`DESAPROBÓ <br>`) : document.write(`APROBÓ <br>`);
+
+
+    document.write(`La nota ingresada de notaCsSc es ${notaCsSc} - `);
+    // If y else de una sola línea
+    if(notaCsSc < 7) document.write(`DESAPROBÓ <br>`);
+    else document.write(`APROBÓ <br>`);
+    
+    promedioLengua = promedioLengua + notaLengua; 
+    promedioMatematica += notaMatematica;
+    promedioCsSc += notaCsSc;
+    
     i++;
 }
 
-console.log("Total: " + total);
 
-
-while(age == false) {
-    age = prompt("¿Cuál es tu edad?");
-    age = parseInt(age);
-}
+promedioLengua = promedioLengua / totalAlumnos;
+promedioMatematica = promedioMatematica / totalAlumnos;
+promedioCsSc = promedioCsSc / totalAlumnos;
+promedioTotal = (promedioCsSc + promedioLengua + promedioMatematica) / 3;
+document.write(`El promedio de los alumnos en MATEMÁTICA es de: ${promedioMatematica} <br>`)
+document.write(`El promedio de los alumnos en LENGUA es de: ${promedioLengua} <br>`)
+document.write(`El promedio de los alumnos en CIENCIAS SOCIALES es de: ${promedioCsSc} <br>`)
+document.write(`El promedio de los alumnos en GENERAL DEL CURSO: ${promedioTotal } <br>`)
