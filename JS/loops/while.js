621b5302cf6bb1529ca8f164051610bcff4a94b6
Martín Rodríguez <neomartinr@gmail.com>
2021-11-01 21:36:45 -0300

    While and do While

--- JS/loops/while.js
+++ JS/loops/while.js
@@ -1,15 +1,40 @@
+let totalAlumnos;
 
-let totalAlumnos = parseInt(prompt("Ingrese el número de alumnos"))
+
+// totalAlumnos = parseInt(prompt("Cantidad de alumnos")); 
+
+// while(isNaN(totalAlumnos)) {
+//     console.log("Entra");
+//     totalAlumnos = parseInt(prompt("Cantidad de alumnos")); 
+//     console.log(isNaN(totalAlumnos))
+// }
+
+do {
+    totalAlumnos = parseInt(prompt("Cantidad de alumnos")); 
+}while(isNaN(totalAlumnos))
+
+console.log(totalAlumnos);
+
 let i = 1;
 let promedioTotal = 0
 let promedioMatematica = 0
 let promedioLengua = 0
 let promedioCsSc = 0
 while(i <= totalAlumnos) {
-    let notaMatematica = +prompt("Ingrese la nota de matemática"); 
-    let notaLengua = +prompt("Ingrese la nota de lengua");
-    let notaCsSc = +prompt("Ingrese la nota de Cs Sociales");
+    let notaMatematica;
+    let notaLengua;
+    let notaCsSc;
+    do{
+        notaMatematica = +prompt("Ingrese la nota de matemática"); //dasdsa  -> NaN
+    }while(notaMatematica > 10 ||  notaMatematica < 1 || isNaN(notaMatematica))
 
+    do{
+        notaLengua = +prompt("Ingrese la nota de lengua");
+    }while(notaLengua > 10 ||  notaLengua < 1 || isNaN(notaLengua));
+
+    do{
+        notaCsSc = +prompt("Ingrese la nota de Cs Sociales");
+    }while(notaCsSc > 10 ||  notaCsSc < 1 || isNaN(notaCsSc));
     document.write(`ALUMNO ${i} <br>  ================================== <br>`);
 
     document.write(`La nota ingresada de matemática es ${notaMatematica} - `);
@@ -27,7 +52,7 @@ while(i <= totalAlumnos) {
     document.write(`La nota ingresada de notaCsSc es ${notaCsSc} - `);
     // If y else de una sola línea
     if(notaCsSc < 7) document.write(`DESAPROBÓ <br>`);
-    else document.write(`APROBÓ <br>`);
+    else document.write(`APROBÓ <br> ================================== <br>`);
     
     promedioLengua = promedioLengua + notaLengua; 
     promedioMatematica += notaMatematica;
