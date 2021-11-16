90f3f5aa2cd8fb1d8e5c275067b71d1da67d7cd9
Martín Rodríguez <neomartinr@gmail.com>
2021-11-15 21:32:51 -0300

    DOM input, values, functions

--- JS/arrays/methods.js
+++ JS/arrays/methods.js
@@ -1,17 +1,32 @@
 let fruits = ["naranja", "manzana", "banana", "kiwi", "frutilla", "uva", "durazno"];
 let fruitOne = fruits[0];
+const resultHTML = document.querySelector('#result');
+const addFruitHTML = document.querySelector('#fruits');
 
-
-let fruitsHTMLElement = document.querySelector('#innerData')
-fruitsHTMLElement.innerHTML = fruits; 
-
-fruitsHTMLElement += `<p>La longitud del array es: ${fruits.length}</p>`
-console.info('Longitud de la', fruitOne, fruitOne[5]);
-
-// console.log(fruits);
-let vocales = ["a", "e", "i", "o", "u"];
-let miTexto = 'Aorem ipsum dolor, sit amet consectetur adipisicing elit. Ex rem voluptatibus corporis, deserunt';
-let contadorVocales = 0;
-for(let i=0; i<miTexto.length; i++){
-    miTexto[i].toLocaleLowerCase()
+function listFruits(){
+    resultHTML.innerHTML = '<h2>Listado de frutas</h2>';
+    console.log('Desde función', fruits);
+    // for(let i=0; i < fruits.length; i++) {
+    //     resultHTML.innerHTML += `${fruits[i]} <br>`;
+    // }
+    fruits.forEach(function(fruit) {
+        resultHTML.innerHTML += `${fruit} <br>`;
+    });
 }
+
+listFruits();
+function addFruit(evt){
+    console.log(evt)
+    if(evt && evt.keyCode !== 13) {
+        return;
+    }
+    // event.preventDefault()
+    const newFruit = addFruitHTML.value;
+    console.log(newFruit);
+    fruits.push(newFruit);
+    listFruits();
+    addFruitHTML.value = '';
+    addFruitHTML.focus();
+}
+
+// console.log(fruits)
